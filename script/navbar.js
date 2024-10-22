8a9e8a8f62218e76e9f19e7027adc3e843065a29
Luka <lukagaxokidze28@gmail.com>
2024-10-22 23:25:26 +0400

    navbar

---
+++ script/navbar.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const navigation = document.getElementById("navigation");
+
+function buildNavigation() {
+  const logoSide = document.createElement("div");
+  const buttonSide = document.createElement("div");
+  const logo = document.createElement("a");
+  const ulWrapper = document.createElement("ul");
+  const burger = document.createElement("i");
+  const loginBtn = document.createElement("a");
+  const registerBtn = document.createElement("a");
+  const loginLi = document.createElement("li");
+  const regLi = document.createElement("li");
+
+  let num = 100;
+
+  if (num > 0) {
+    // build logo side
+    logo.classList.add("logo");
+    logo.textContent = "Shop";
+    logo.href = "/";
+
+    logoSide.classList.add("logoSide");
+    logoSide.appendChild(logo);
+
+    navigation.appendChild(logoSide);
+
+    // building buttons side
+    ulWrapper.classList.add("nav-items");
+    burger.classList.add("headerBar", "fa-solid", "fa-bars");
+
+    loginBtn.classList.add("nav-item");
+    loginBtn.textContent = "login";
+    loginBtn.href = "auth";
+
+    registerBtn.classList.add("nav-item");
+    registerBtn.textContent = "register";
+    registerBtn.href = "auth";
+
+    ulWrapper.classList.add("nav-items");
+    buttonSide.classList.add("buttonSide");
+    loginLi.appendChild(loginBtn);
+    regLi.appendChild(registerBtn);
+    ulWrapper.appendChild(loginLi);
+    ulWrapper.appendChild(regLi);
+    buttonSide.appendChild(burger);
+    buttonSide.appendChild(ulWrapper);
+    navigation.appendChild(buttonSide);
+
+    const burgerBar = document.querySelector(".headerBar");
+
+    burgerBar.addEventListener("click", function () {
+      ulWrapper.classList.toggle("active");
+      if (burgerBar.classList.contains("fa-bars")) {
+        burgerBar.classList.replace("fa-bars", "fa-x");
+      } else {
+        burgerBar.classList.replace("fa-x", "fa-bars");
+      }
+    });
+
+    window.addEventListener("resize", function () {
+      if (window.innerWidth > 768) {
+        ulWrapper.classList.remove("active");
+      }
+    });
+  } else {
+  }
+}
+
+buildNavigation();
