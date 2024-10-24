19f52013eb527ff7093fd106708cc6fd1ba0a8c7
Luka <lukagaxokidze28@gmail.com>
2024-10-24 21:28:34 +0400

    working on category

--- script/main.js
+++ script/main.js
@@ -1,8 +1,19 @@
 const arrow = document.querySelectorAll(".arrow");
 const carusel = document.querySelector(".carusel");
 const categories = document.querySelector(".categories");
+const dropBrands = document.querySelector(".categoryBtn");
 
 init();
+
+// fetch(
+//   "https://api.everrest.educata.dev/shop/products/all?page_index=1&page_size=50"
+// )
+//   .then((res) => res.json())
+//   .then((res) => {
+//     console.log(res);
+//   });
+
+// https:api.everrest.educata.dev/shop/products/category/2?page_size=10
 
 function init() {
   getInfo();
@@ -24,7 +35,7 @@ function getInfo() {
       return res.json();
     })
     .then((res) => {
-      console.log(res);
+      // console.log(res);
     })
     .catch((err) => {
       console.log(err);
@@ -66,6 +77,59 @@ function getCategories() {
         li.appendChild(categoryBtn);
         categories.appendChild(li);
       });
+    });
+}
+
+buildCategori();
+
+async function buildCategori() {
+  const res = await fetch(
+    "https://api.everrest.educata.dev/shop/products/categories"
+  );
+  const parseRes = await res.json();
+
+  const li = document.querySelectorAll(".categoryLi");
+
+  parseRes.forEach((categoriID, index) => {
+    if (li[index]) {
+      li[index].setAttribute("category", `${categoriID.id}`);
+      console.log(categoriID);
+    }
+  });
+
+  li.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      const categoryid = btn.getAttribute("category");
+      if (btn.classList.contains("expanded")) {
+        const categoriItem = document.createElement("p");
+        specificInfo(
+          `https://api.everrest.educata.dev/shop/products/category/${categoryid}?page_size=10`,
+          function (res) {
+            const brands = res.products.map((element) => element.brand);
+
+            const checkDuplicates = brands.filter(
+              (brand, index, self) => self.indexOf(brand) === index
+            );
+
+            console.log(checkDuplicates);
+          }
+        );
+      }
+    });
+  });
+}
+function specificInfo(url, callBack) {
+  fetch(url, {
+    method: "GET",
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Error");
+      }
+      return res.json();
+    })
+    .then((res) => {
+      callBack(res);
     });
 }
 
